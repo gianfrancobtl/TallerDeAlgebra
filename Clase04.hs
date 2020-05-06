c304494cda8b731a28d117584d4b44a39bc3347a
Gianfranco <gianfrancobtl@gmail.com>
2020-05-06 11:16:05 -0300

    Clase 04 terminada

--- Clase04.hs
+++ Clase04.hs
@@ -1,5 +1,6 @@
 module Clase04
 where
+import Clase03
 
 sumatoria :: Int -> Int
 sumatoria 0 = 0
@@ -59,3 +60,41 @@ sumaPotencias q n m = (sumaPotencias q n (m-1)) + q^m * (f2 n q)
 sumaRacionales ::  Int -> Int -> Float
 sumaRacionales n 0 = 0
 sumaRacionales n m = (sumaRacionales n (m-1)) + (fromIntegral(sumatoria n)) / (fromIntegral m)
+
+-- EJERCICIOS DE TAREA --
+-- EJERCICIO 4 --
+g1 :: Int -> Int -> Int
+g1 i n | n < i =0
+       | otherwise = i^n + g1 i (n-1)
+
+-- EJERCICIO 5 --
+-- suma i^n dssde i=1 hasta n (g1 "al revés").
+hAzul :: Int -> Int -> Int
+hAzul 1 n = 1
+hAzul i n = i^n + hAzul (i-1) n
+
+-- suma de i^j i desde 1 hasta n, j desde i hasta n.
+g2 :: Int -> Int
+g2 1 = 1
+g2 n = g2 (n-1) + hAzul n n
+
+-- OTRA MANERA --
+-- suma desde i=1 hasta n (g1 i m)
+g2Aux :: Int -> Int -> Int
+g2Aux 1 m = g1 1 m
+g2Aux n m = g1 n m + g2Aux (n-1) m
+
+g2bis :: Int -> Int
+g2bis n = g2Aux n n
+
+-- EJERCICIO 6 --
+-- g3 chequea que n sea par, otherwise vuelve a llamarla para n-1, es decir, con un n par.
+g3 :: Int -> Int
+g3 n | n `mod` 2 == 0 =g3 (n-1) + 2^n
+     | otherwise = g3 (n-1)
+
+-- EJERCICIO 7 --
+g4 :: Int -> Int
+g4 0 = 0
+g4 n | digitosIguales n = n + g4 (n-1)
+     | otherwise = g4 (n-1)
