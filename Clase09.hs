2d7a80f8d576c1388dec06e50b7e712f81430af6
Gianfranco <gianfrancobtl@gmail.com>
2020-06-26 12:42:36 -0300

    Clase 9

--- Clase09.hs
+++ Clase09.hs
@@ -38,20 +38,30 @@ numero [a] _ = a
 numero l  n = (last l)*n^j + numero (init l) n
     where j = length(l)-1
 
+-- Otras opciones
+numero' :: [Int] -> Int -> Int
+numero' (x:xs) b = numeroaux (x:xs) b 0
+    where numeroaux :: [Int] -> Int -> Int -> Int
+          numeroaux [] b i = 0
+          numeroaux (x:xs) b i = numeroaux xs b (i+1) + x*(b^i)
+
+numero'' :: [Int] -> Int -> Int
+numero'' [] b = 0
+numero'' (x:xs) b = x + (numero xs b) * b
+
 -- MCD
 -- Posible algoritmo por def:
 mcdDef :: Int -> Int ->  Int
 mcdDef a 0 = abs a
 mcdDef 0 b = abs b
 mcdDef a b = maximo (interseccion (divisores a) (divisores b))
-
+    
 divisores :: Int -> Set Int
 divisores n = listaDivisores n n
-
-listaDivisores :: Int -> Int -> Set Int
-listaDivisores n 0 = []
-listaDivisores n m | n `mod` m == 0 = m:listaDivisores n (m-1)
-                   | otherwise      = listaDivisores n (m-1)
+    where listaDivisores :: Int -> Int -> Set Int
+          listaDivisores n 0 = []
+          listaDivisores n m | n `mod` m == 0 = m:listaDivisores n (m-1)
+                             | otherwise      = listaDivisores n (m-1)
 
 -- Algoritmo de Euclides
 mcd :: Int -> Int -> Int
@@ -67,9 +77,26 @@ mcm' :: Int -> Int -> Int
 mcm' 0 0 = 0
 mcm' a b = a * b `div` (mcd a b)
 
+-- mcm'' :: Int -> Int -> Int
+-- mcm'' 0 0 = 0
+-- mcm'' a b = mcmDesde a b 1
+
+-- mcmDesde :: Int -> Int -> Int -> Int
+-- mcmDesde a b i | (a `divideA` i) && (b `divideA` i) =i
+--                | otherwise = mcmDesde a b (i+1)
+ 
 -- ej 1
 emcd :: Int -> Int -> (Int, Int, Int)
-emcd a b = (mcd a b):(generaSyT a b)
+emcd a 0 = (abs a, signum a, 0)
+emcd a b = (mcd, tau, sigma - (a `div` b) * tau)
+    where (mcd, sigma, tau) = (emcd b (a `mod` b))
 
--- generaSyT :: Int -> Int -> (Int, Int)
--- generaSyT a b = 
+emcdMin :: Int -> Int ->  (Int, Int, Int)
+emcdMin a b = emcdMinDesde a b 0
+
+emcdMinDesde :: Int -> Int -> Int -> (Int, Int, Int)
+emcdMinDesde a b k | (s < 0) && (s - k * (b `div` mcd) >=0) = (mcd, s - k * (b `div` mcd), t + k * (a `div` mcd))
+                   | (s >= 0) && (s - (k+1) * (b `div` mcd) < 0) = (mcd, s-k * (b `div` mcd), t + k * (a `div` mcd))
+                   | (s < 0) = emcdMinDesde a b (k-1)
+                   | (s >= 0) = emcdMinDesde a b (k+1)
+    where (mcd, s, t) = emcd a b
