88421c402557fa33031f77085210681939129fe3
Gianfranco <gianfrancobtl@gmail.com>
2020-07-08 11:40:02 -0300

    Terminada

--- Clase11.hs
+++ Clase11.hs
@@ -23,19 +23,21 @@ inverso (a,b) = (a/(a**2 + b**2),(-b)/(a**2 + b**2))
 
 -- Ejercicios --
 cociente :: Complejo -> Complejo -> Complejo
-cociente (a, b) (c, d) = ( (a*c + b*d) / (c**2+d**2) , (b*c - a*d) / (c**2 + d**2) )
+cociente (a, b) (c, d) = ( real , imaginario )
+      where real =(a*c + b*d) / (c**2+d**2)
+            imaginario = (b*c - a*d) / (c**2 + d**2)
 
 potencia :: Complejo -> Integer -> Complejo
-potencia z 1      = z
-potencia (a, b) n = producto (a,b) (potencia (a,b) (n-1))
+potencia z 0      = (1, 0)
+potencia z n = producto z (potencia z (n-1))
 
 solucionesCuadratica :: Float -> Float -> Float -> (Complejo, Complejo)
 solucionesCuadratica a b c | haySolucionesEnR = ( ( n + cerosReales, 0), (n - cerosReales, 0) )
                            | otherwise  = ( (n, cerosImaginarios ) , (n , -cerosImaginarios) )
       where haySolucionesEnR = (b**2 - 4*a*c >= 0)
             n = (-b/(2*a))
-            cerosReales = ((sqrt(b**2 - 4*a*c))/(2*a))
-            cerosImaginarios = ((sqrt(4*a*c - b**2))/(2*a))
+            cerosReales = (sqrt(b**2 - 4*a*c))/(2*a)
+            cerosImaginarios = (sqrt(4*a*c - b**2))/(2*a)
 -- -- -- -- -- --
 
 modulo :: Complejo -> Float
@@ -63,13 +65,11 @@ raizCuadrada z=(pasarACartesianas r tita ,pasarACartesianas r (tita+pi))
 
 -- Ejercicio
 solucionesCuadraticaCoefComplejos :: Complejo -> Complejo -> Complejo -> (Complejo, Complejo)
-solucionesCuadraticaCoefComplejos c w z = (( wInverso `suma` raizCompleja), wInverso `resta` raizCompleja)
-      where wInverso = (-re w,-im w) `cociente` (productoRealComplejo 2 c)
+solucionesCuadraticaCoefComplejos c w z = (x1,x2)
+      where wInverso = (-re w,-im w) `cociente` ( (2,0) `producto` c)
             raizPositiva (a,b) = a
-            raizCompleja = raizPositiva(raizCuadrada( (w `potencia` 2) `resta` ( productoRealComplejo 4 ( c `producto` z )))) `cociente` (productoRealComplejo 2 c)
-
-productoRealComplejo :: Float -> Complejo -> Complejo
-productoRealComplejo n (a,b) = (n*a,n*b)
+            raizCompleja = raizPositiva(raizCuadrada( (w `potencia` 2) `resta` ( (4, 0) `producto` ( c `producto` z )))) `cociente` ( (2,0) `producto` c)
+            (x1,x2) = ( wInverso `suma` raizCompleja, wInverso `resta` raizCompleja)
 --
 
 raicesNEsimas :: Integer -> [Complejo]
@@ -84,6 +84,10 @@ raicesNEsimasDesde k n | k>= n = []
 -- Dados k y n enteros con 0 ≤ k < n, devuelve la lista con las potencias 0, 1, . . . , n − 1 de la raız
 -- n-esima asociada a k siguiendo la formula de arriba.
 potenciasRaizNEsima :: Integer -> Integer -> [Complejo]
-potenciasRaizNEsima k 0 = []
-potenciasRaizNEsima k n = (potencia (kesimaRaiz) (n-1)) : potenciasRaizNEsima k (n-1)
+potenciasRaizNEsima n k = potenciasRaizNEsimaHasta n k n
+
+potenciasRaizNEsimaHasta  :: Integer -> Integer -> Integer -> [Complejo]
+potenciasRaizNEsimaHasta n k i | i < 0 = []
+                               | otherwise = ( potencia kesimaRaiz (i) ) : (potenciasRaizNEsimaHasta n k (i-1) )
   where kesimaRaiz =(cos (2*(fromInteger k)*pi/(fromInteger n)) , sin ((2*(fromInteger k)*pi)/(fromInteger n)))
+-- -- --
